c24a41334f5f40028f607ea7afd5dc1aa380b1a9
FARACE cristian <cristianfarace@gmail.com>
2022-07-16 20:03:45 -0300

    agregue comentario en onlyhtmlcss y hago commit en proyecto_final

--- src/database.js
+++ src/database.js
@@ -2,3 +2,5 @@ const mongoose = require ("mongoose")
 mongoose.connect ('mongodb+srv://cristianfarace:rojo1994@cluster0.war4lia.mongodb.net/?retryWrites=true&w=majority')
 .then (()=> {console.log ("Conectado a la base de datos MONGODB de forma exitosa")})
 .catch ((err)=> {console.log ("error", err)})
+
+// comentario
