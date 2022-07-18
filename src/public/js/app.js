ea8b8aae188965de02ca7274f69013b47abb12cb
FARACE cristian <cristianfarace@gmail.com>
2022-07-18 16:13:00 -0300

    el nav lo puse en amarillo el color en black, separe el carrito del nav y le puse un fondo blanco transparente

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -41,14 +41,6 @@ const pintarProductos = (data) => {
 
 
 
-// (1) una vez cargada la pagina se hace un fetch pidiendo todos los productos
-// (2) luego se le pregutna al localStorage si tiene algo en el carrito  y en caso afirmativo que lo pinte
-// (3) la funcion pintar productos ============> lo que hace es que por cada producto que llega de la peticion fetch forma una tarjeta, cada tarjeta se va insetando al fragment y una vez creadas todas las tarjetas, le inserto al DOM el fragment
-// (4) En el paso anteriror al boton agregar de cada producto se le vincula el id del producto mediante el atributo data
-// (5) con la funcion detectar botones ============> recorro cada boton, a cada boton se le agrega una escucha de evento evento, (con un add event listener, no se usa onclic pq es viejo eso) por lo que, cada vez que se le haga clic a un boton comprar,voy a empujar ese producto al carrito de compras. ¿Como funciona el carrito?
-
-//El carrito es un objeto que COLECCIONA DATOS ORDENADOS POR UN VALOR DE INDICE. Ej 1:{id:1, titulo:'cafe', precio:500, cantidad: 1}. Por cada clic que se haga en comprar, se agrega un indice con un objeto que dentro tiene las propiedaes de esa tarjeta. Si en el carrito hay 2 productos iguales la popiedad cantidad aumenta en 1. Otra forma de hacerlo es almacenarlo en un array, pero aca uso un objeto. Al usar un objeto me permite crear el inidice que mencionaba antes. El beneficio de trabajar con objetos es que no tengo que recorrer todo el carrito para manipular los botones de accion del carrito ( boton + y boton -) , porque puedo acceder a cada indicie en particular y cada vez que se agregue desde el carrito un producto o vuelva a apretar compar en ese producto desde la tarjeta, lo que hace el programa es modificar netamente el objeto en cuestion accediendo a travez del indice. AUnque igualmente lo puedo hace con array y agregarle un indicie. Para poder pintar el contenido del carrito tengo 2 opciones 1 es usar el for in (for const key in carrito) hace tal cosa o sino con Object.Values con un forEach (lo que hace object values es transformar el objeto en un array)
-
 
 let carrito = {}
 const detectarBotones = (data) => {
@@ -60,7 +52,7 @@ const detectarBotones = (data) => {
             // console.log(btn.dataset.id)
             const producto = data.find( item  => item.id === parseInt(btn.dataset.id) )
             producto.cantidad = 1
-//si ya existe en carrito... aumento su cantidad
+//si ya existe en carrito... aumento su la propiedad cantidad de ese objeto
             if (carrito.hasOwnProperty(producto.id)) {
                 producto.cantidad = carrito[producto.id].cantidad + 1
             }
@@ -72,19 +64,31 @@ const detectarBotones = (data) => {
     })
 }
 
+// (1) una vez cargada la pagina se hace un fetch pidiendo todos los productos
+// (2) luego se le pregutna al localStorage si tiene algo en el carrito  y en caso afirmativo que lo pinte
+// (3) la funcion pintar productos ============> lo que hace es que por cada producto que llega de la peticion fetch forma una tarjeta, cada tarjeta se va insetando al fragment y una vez creadas todas las tarjetas, le inserto al DOM el fragment
+// (4) En el paso anteriror al boton agregar de cada producto se le vincula el id del producto mediante el atributo data
+// (5) con la funcion detectar botones ============> recorro cada boton, a cada boton se le agrega una escucha de evento evento, (con un add event listener, no se usa onclic pq es viejo eso) por lo que, cada vez que se le haga clic a un boton comprar,voy a empujar ese producto al carrito de compras. EL carrito de comprar NO PINTA NADA, solamente almcena los datos de los productos, para luego poder recorrerlos y pintarlos en el DOM de mi pagina ¿Como funciona el carrito?
+
+//El carrito es un objeto que COLECCIONA DATOS ORDENADOS POR UN VALOR DE INDICE. Ej 1:{id:1, titulo:'cafe', precio:500, cantidad: 1}. Por cada clic que se haga en comprar, se agrega un indice con un objeto que dentro tiene las propiedaes de esa tarjeta. Si en el carrito hay 2 productos iguales la popiedad cantidad aumenta en 1. Otra forma de hacerlo es almacenarlo en un array, pero aca uso un objeto. Al usar un objeto me permite crear el inidice que mencionaba antes. El beneficio de trabajar con objetos es que no tengo que recorrer todo el carrito para manipular los botones de accion del carrito ( boton + y boton -) , porque puedo acceder a cada indicie en particular y cada vez que se agregue desde el carrito un producto o vuelva a apretar compar en ese producto desde la tarjeta, lo que hace el programa es modificar netamente el objeto en cuestion accediendo a travez del indice. AUnque igualmente lo puedo hace con array y agregarle un indicie. Para poder pintar el contenido del carrito tengo 2 opciones 1 es usar el for in (for const key in carrito) hace tal cosa o sino con Object.Values con un forEach (lo que hace object values es transformar el objeto en un array)
+
+// (6) con la funcion pintar carrito ============> una vez que termina de recorrer y pintar el carrito, inserto en el fragmento en el DOM a travez el div con el id Items
+
 const items = document.querySelector('#items')
 
 const pintarCarrito = () => {
 
     //pendiente innerHTML
     items.innerHTML = ''
-
+    
     const template = document.querySelector('#template-carrito').content
     const fragment = document.createDocumentFragment()
 
+//como no se puede recorrer un objeto con un forEach el object.values lo que hace es transformar ese objeto en un array, y una vez transformado en un array lo puedo recorrer con un for Eeach
     Object.values(carrito).forEach(producto => {
         // console.log('producto', producto)
         template.querySelector('th').textContent = producto.id
+        //cuando tengo muchos TD para acceder al primero hago td [0]
         template.querySelectorAll('td')[0].textContent = producto.title
         template.querySelectorAll('td')[1].textContent = producto.cantidad
         template.querySelector('span').textContent = producto.precio * producto.cantidad
@@ -107,19 +111,25 @@ const pintarCarrito = () => {
 const footer = document.querySelector('#footer-carrito')
 const pintarFooter = () => {
 
-    footer.innerHTML = ''
+    footer.innerHTML = '' //esto lo que hace es limpiar el html
 
     if (Object.keys(carrito).length === 0) {
         footer.innerHTML = `
         <th scope="row" colspan="5">Carrito vacío. Comience a comprar</th>
         `
+        const pagar = document.getElementById ('pagar')
+        pagar.style.backgroundColor='yellow'
         return
     }
 
+    const pagar = document.getElementById ('pagar')
+    pagar.style.backgroundColor='green'
+    pagar.style.borderRadius= '10px'
     const template = document.querySelector('#template-footer').content
     const fragment = document.createDocumentFragment()
 
     // sumar cantidad y sumar totales
+    //reduce es una funcion de flecha que tiene un acumulador, una propiedad, la operacion que voy a realizar con el acumulador y la cantidad y en que formato me va a devolver la respuesta. EN este caso toma un acumulador (acc,)  se puede llamar como yo quiera y toma un elemento al que le suma cosas... en este caso como suma una propiedad de un objeto va entre llaves cantidad. De esta forma la cantidad se va a ir acumulando al contador, entonces la primer vuelta el acumulador va a ser 0, cantidad va a ser 1 y se lo suma al acumulador. En la proxima vuelta el acumulador va a ser 1 y la cantidad 2, y asi sucesivamente
     const nCantidad = Object.values(carrito).reduce((acc, { cantidad }) => acc + cantidad, 0)
     const nPrecio = Object.values(carrito).reduce((acc, {cantidad, precio}) => acc + cantidad * precio ,0)
     // console.log(nPrecio)
